a7cfc756ca9d4237993eb1b130afab3997bd5dd4
Luis Rafael Diaz Anaya <53496639+luisdico313@users.noreply.github.com>
2025-05-05 11:04:41 -0500

    Main actualizado

--- Actividad 2/Ejercicio5_25_95/src/main/java/com/mycompany/ejercicio5_25_95/Ejercicio5_25_95.java
+++ Actividad 2/Ejercicio5_25_95/src/main/java/com/mycompany/ejercicio5_25_95/Ejercicio5_25_95.java
@@ -11,6 +11,10 @@ package com.mycompany.ejercicio5_25_95;
 public class Ejercicio5_25_95 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CuentaBancaria cuenta = new CuentaBancaria(“Pedro”,”Pérez”,123456789,tipo.AHORROS);
+        cuenta.imprimir();
+        cuenta.consignar(200000);
+        cuenta.consignar(300000);
+        cuenta.retirar(400000);
     }
 }
