56d61778286c475b2814e59d92e54773c5c1dcad
Luis Rafael Diaz Anaya <53496639+luisdico313@users.noreply.github.com>
2025-05-05 11:09:03 -0500

    Clase Cuenta Bancaria creada

---
+++ Actividad 2/Ejercicio5_25_95/src/main/java/com/mycompany/ejercicio5_25_95/CuentaBancaria.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio5_25_95;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CuentaBancaria {
+    
+}
