d83662004ea732220beac286cd9381fc34f95789
Luis Rafael Diaz Anaya <53496639+luisdico313@users.noreply.github.com>
2025-05-05 10:57:57 -0500

    Prueba
    Verificación

---
+++ Actividad 2/Ejercicio5_25_95/src/main/java/com/mycompany/ejercicio5_25_95/Ejercicio5_25_95.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio5_25_95;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejercicio5_25_95 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
