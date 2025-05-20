338a1a9a797d569f3607edb5ce2ef983914818f3
Luis Rafael Diaz Anaya <53496639+luisdico313@users.noreply.github.com>
2025-05-19 23:15:02 -0500

    Creación Carpeta y Primer Proyecto

---
+++ Actividad 3/Ejercicio1_4_1_194/Ejercicio1_4_1_194/src/main/java/com/mycompany/ejercicio1_4_1_194/Ejercicio1_4_1_194.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio1_4_1_194;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejercicio1_4_1_194 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
