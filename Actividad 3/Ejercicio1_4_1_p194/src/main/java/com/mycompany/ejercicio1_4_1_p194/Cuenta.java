900b148005bd930fa7a5c7b6f47e1b6598fcf313
Luis Rafael Diaz Anaya <53496639+luisdico313@users.noreply.github.com>
2025-05-20 22:16:11 -0500

    Ejercicio 1 Actualizado

--- Actividad 3/Ejercicio1_4_1_p194/src/main/java/com/mycompany/ejercicio1_4_1_p194/Cuenta.java
+++ Actividad 3/Ejercicio1_4_1_p194/src/main/java/com/mycompany/ejercicio1_4_1_p194/Cuenta.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.ejercicio1_4_1_p194;
 
 public class Cuenta {
--- Actividad 3/Ejercicio1_4_1_p194/src/main/java/com/mycompany/ejercicio1_4_1_p194/Ejercicio1_4_1_p194.java
+++ Actividad 3/Ejercicio1_4_1_p194/src/main/java/com/mycompany/ejercicio1_4_1_p194/Ejercicio1_4_1_p194.java
@@ -1,6 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.ejercicio1_4_1_p194;
 
