99676e93961bf6f177b804be39ed416f3f22fc60
Luis Rafael Diaz Anaya <53496639+luisdico313@users.noreply.github.com>
2025-05-20 21:28:48 -0500

    Proyecto creado

---
+++ Actividad 3/Ejercicio5_4_4_p231/Ejercicio5_4_4_p231/src/main/java/Profesores/Ejercicio5_4_4_p231.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Profesores;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejercicio5_4_4_p231 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
